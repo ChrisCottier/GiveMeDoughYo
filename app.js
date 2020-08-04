07b0a361d4d6b76b6471fe3359c38a3eb4987757
chris <cgcottier@gmail.com>
2020-08-04 16:34:04 -0500

    heroku bugfix

--- app.js
+++ app.js
@@ -17,13 +17,12 @@ app.use(cors({ origin: frontendUrl }));
 
 app.use("/api", router);
 
-
 //before error handling
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('front-end/build'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("front-end/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 module.exports = app;
