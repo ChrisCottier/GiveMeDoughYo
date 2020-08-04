371ce2086b15b7036b32ca1ece0cebeae7f72213
chris <cgcottier@gmail.com>
2020-08-04 17:06:07 -0500

    changed bin www

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,6 +1,6 @@
 export const backendUrl =
   process.env.REACT_APP_BACKEND_URL || process.env.NODE_ENV === "production"
-    ? "https://givemedoughyo.herokuapp.com/"
+    ? "https://givemedoughyo.herokuapp.com"
     : "http://localhost:8080";
 export const baseUrl =
   process.env.REACT_APP_BACKEND_BASEURL || `${backendUrl}/api`;
