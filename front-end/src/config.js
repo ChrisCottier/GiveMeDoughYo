4bcd13d3eb1c05a363846b310ede21166a09619b
chris <cgcottier@gmail.com>
2020-08-04 15:51:37 -0500

    config updated for heroku

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,5 +1,7 @@
 export const backendUrl =
-  process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
+  process.env.REACT_APP_BACKEND_URL || process.env.NODE_ENV === "production"
+    ? "https://givemedoughyo.herokuapp.com/"
+    : "http://localhost:8080";
 export const baseUrl =
   process.env.REACT_APP_BACKEND_BASEURL || `${backendUrl}/api`;
 export const appName = "GiveMeDoughYo";
