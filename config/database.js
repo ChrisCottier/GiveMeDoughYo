158921e9fbc7f8e8f8e24b20166c85f19239ba1e
chris <cgcottier@gmail.com>
2020-08-04 15:38:27 -0500

    fixed backend module bug

--- config/database.js
+++ config/database.js
@@ -11,10 +11,6 @@ module.exports = {
     seederStorage: "sequelize",
   },
   production: {
-    // username,
-    // password,
-    // database,
-    // host,
     dialect: "postgres",
     seederStorage: "sequelize",
     use_env_variable: "DATABASE_URL",
