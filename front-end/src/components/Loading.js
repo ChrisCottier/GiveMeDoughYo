1130f9badaa4529233a2344cb801933a34abf78d
Chris Cottier <cgcottier@gmail.com>
2020-12-13 10:44:40 -0600

    added loading component for user and campaing pages

---
+++ front-end/src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <main className="centered">
+            <img src="https://indiegogo-clone.s3.us-east-2.amazonaws.com/Spinner-1s-200px.gif"></img>
+        </main>
+    )
+}
+
+export default Loading;
